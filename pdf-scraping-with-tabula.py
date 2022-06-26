29ac3706ba9ccb23ea000930d17e37cac759f70e
MarcuzAC <sambanankhumhango@gmail.com>
2022-06-26 05:46:26 +0200

    Update pdf-scraping-with-tabula.py

--- pdf-scraping-with-tabula.py
+++ pdf-scraping-with-tabula.py
@@ -3,4 +3,3 @@ import pandas as pd
 
 pdf_path = r"C:\Users\pandas\Pictures\Certificates\World Countries and Capital & Languages.pdf"
 df = tabula.read_pdf(pdf_path, pages = "all", stream = True)
-# !cat test.csv
